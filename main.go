cc2e76bf5af1942e7d3b0f8b4a86b9c1bcdf0616
Okike Chinonso <Chinonsookike@gmail.com>
2022-02-27 01:17:43 +0100

    graphql crud added

--- main.go
+++ main.go
@@ -3,107 +3,208 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"strconv"
+	"math/rand"
+	"net/http"
+	"time"
 
 	"github.com/graphql-go/graphql"
 )
 
-func main() {
-	tutorials := Populate()
-	var commentType = graphql.NewObject(
-		graphql.ObjectConfig{
-			Name: "Comment",
-			Fields: graphql.Fields{
-				"body": &graphql.Field{
-					Type: graphql.String,
-				},
+type Product struct {
+	ID    int64   `json:"id"`
+	Name  string  `json:"name"`
+	Info  string  `json:"info,omitempty"`
+	Price float64 `json:"price"`
+}
+
+var products = []Product{
+	{
+		ID:    1,
+		Name:  "Chicha Morada",
+		Info:  "Chicha morada is a beverage originated in the Andea regions of Peru but is actually consumed at a national level (wiki)",
+		Price: 7.99,
+	},
+	{
+		ID:    2,
+		Name:  "Chicha de jora",
+		Info:  "Chicha de jora is a corn beer chicha prepared by germinating maize, extracting the malt sugars, boiling the wort, and fermenting it in large vessels (traditionally huge earthenware vats) for several days (wiki)",
+		Price: 5.95,
+	},
+}
+
+var productType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Product",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"info": &graphql.Field{
+				Type: graphql.String,
+			},
+			"price": &graphql.Field{
+				Type: graphql.Float,
 			},
 		},
-	)
+	},
+)
 
-	var authorType = graphql.NewObject(
-		graphql.ObjectConfig{
-			Name: "Author",
-			Fields: graphql.Fields{
-				"Name": &graphql.Field{
-					Type: graphql.String,
+var queryType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"product": &graphql.Field{
+				Type:        productType,
+				Description: "Get product by id",
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.Int,
+					},
 				},
-				"Tutorial": &graphql.Field{
-					Type: graphql.NewList(graphql.Int),
-				},
-			},
-		},
-	)
-
-	var tutorialType = graphql.NewObject(
-		graphql.ObjectConfig{
-			Name: "Tutorial",
-			Fields: graphql.Fields{
-				"id": &graphql.Field{
-					Type: graphql.Int,
-				},
-				"title": &graphql.Field{
-					Type: graphql.String,
-				},
-				"author": &graphql.Field{
-					Type: authorType,
-				},
-				"comments": &graphql.Field{
-					Type: graphql.NewList(commentType),
-				},
-			},
-		},
-	)
-
-	fields := graphql.Fields{
-		"tutorial": &graphql.Field{
-			Type: tutorialType,
-			Args: graphql.FieldConfigArgument{
-				"id": &graphql.ArgumentConfig{
-					Type: graphql.Int,
-				},
-			},
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				id, ok := p.Args["id"].(int)
-				if ok {
-					for _, tutorial := range tutorials {
-						i,_:= strconv.Atoi(tutorial.ID)
-						if i == id{
-							return tutorial,nil
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					id, ok := p.Args["id"].(int)
+					if ok {
+						for _, product := range products {
+							if int(product.ID) == id {
+								return product, nil
+							}
 						}
 					}
-				}
-				return nil, nil
+					return nil, nil
+				},
+			},
+			"list": &graphql.Field{
+				Type:        graphql.NewList(productType),
+				Description: "Get product list",
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return products, nil
+				},
 			},
 		},
-		"list": &graphql.Field{
-			Type: graphql.NewList(tutorialType),
-			Description: "Get full tutorial list",
+	},
+)
+
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"create": &graphql.Field{
+			Type:        productType,
+			Description: "Create new product",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"info": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"price": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Float),
+				},
+			},
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return tutorials,nil 
+				rand.Seed(time.Now().UnixNano())
+				product := Product{
+					ID: int64(rand.Intn(10000)),
+					Name: p.Args["name"].(string),
+					Info: p.Args["info"].(string),
+					Price: p.Args["price"].(float64),
+				}
+				products = append(products, product)
+				return product, nil 
 			},
 		},
+		"update": &graphql.Field{
+			Type: productType,
+			Description: "Update product by id",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"info": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"price": &graphql.ArgumentConfig{
+					Type: graphql.Float,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				id, _ := p.Args["id"].(int)
+				name, nameOk := p.Args["name"].(string)
+				info, infoOk := p.Args["info"].(string)
+				price, priceOk := p.Args["price"].(float64)
+				product := Product{}
+				for i,p := range products{
+					if int64(id) == p.ID{
+						if nameOk{
+							products[i].Name = name 
+						}
+						if infoOk{
+							products[i].Info = info 
+						}
+						if priceOk {
+							products[i].Price = price
+						}
+						product = products[i]
+						break
+					}
+				}
+				return product,nil
+			},
+		},
+		"delete": &graphql.Field{
+			Type:        productType,
+			Description: "Delete product by id",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, _ := params.Args["id"].(int)
+				product := Product{}
+				for i, p := range products {
+					if int64(id) == p.ID {
+						product = products[i]
+						// Remove from product list
+						products = append(products[:i], products[i+1:]...)
+					}
+				}
+
+				return product, nil
+			},
+		},
+	},
+})
+
+var schema,_ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query: queryType,
+		Mutation: mutationType,
+	},
+)
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result{
+	result := graphql.Do(graphql.Params{
+		Schema: schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("errors: %v",result.Errors)
 	}
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemeConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, _ := graphql.NewSchema(schemeConfig)
-	// err != nil {
-	// 	log.Fatalf("Error creating schema",err)
-	// }
-	query := `{ 
-		tutorial(:id)
-		list{
-			id
-			title
-			author
-		}
-		}`
-	params := graphql.Params{Schema: schema, RequestString: query}
-	r := graphql.Do(params)
-	if len(r.Errors) > 0 {
-		log.Fatalf("fail", r.Errors)
-	}
-	rJson, _ := json.Marshal(r)
-	fmt.Printf("%s ", rJson)
+	return result
+}
+
+func main(){
+	http.HandleFunc("/product", func(rw http.ResponseWriter, r *http.Request) {
+		result := executeQuery(r.URL.Query().Get("query"),schema)
+		json.NewEncoder(rw).Encode(result)
+	})
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080",nil)
 }
